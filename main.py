931e659dd969ab28a64779625607c7f58d09d656
Hodor <rooomka_kostelov@mail.ru>
2026-07-27 14:21:54 +0300

    first exercise

--- main.py
+++ main.py
@@ -23,18 +23,30 @@ class TaskSchema(BaseModel):
 class TaskCreateSchema(BaseModel):
     title: str
 
-tasks: list[TaskSchema] = []
+class BookSchema(BaseModel):
+    book: str
 
+
+tasks: list[TaskSchema] = []
+book = ''
 def read_base_page():
     return {"message": "Hello World"}
 
 
 @app.get("/tasks")
-def read_tasks() -> list[TaskSchema]:
-    return tasks
+def read_tasks() -> str:
+    return f'Любимая книга: {book}'
 
 @app.post("/tasks")
 def create_task(payload: TaskCreateSchema) -> TaskSchema:
+    print(payload)
     new_task = TaskSchema(id=str(uuid4()), title=payload.title, completed=False)
     tasks.append(new_task)
     return new_task
+
+@app.post("/books")
+def create_book(payload: BookSchema) -> BookSchema:
+    global book
+    book = payload.book
+    return BookSchema(book=book)
+
